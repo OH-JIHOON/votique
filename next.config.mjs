2c981d9bcd141b4e1aa2de008310d421a49ebe95
OH-JIHOON <abve@me.com>
2025-03-14 17:47:00 +0900

    Fix: Hydration Mismatch & Firebase Analytics 오류 수정

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    experimental: {
+      appDir: true, // Next.js 15+에서 App Router 활성화
+      turboMode: true, // Turbopack 활성화
+    },
+    webpack: (config) => {
+      config.resolve.fallback = { fs: false, path: false };
+      return config;
+    },
+  };
+  
+  module.exports = nextConfig;
